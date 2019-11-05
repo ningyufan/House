076d5a31dbc4df6974e4696f0d9d308ea425165c
ningyufan <223_ningyufan@computer162.com>
2019-11-05 15:36:47 +0800

    22

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const webpack = require('webpack');
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
@@ -11,7 +12,13 @@ module.exports = {
     // Copy our app's index.html to the build folder.
     new CopyWebpackPlugin([
       { from: './app/index.html', to: "index.html" }
-    ])
+    ]),
+    new webpack.DefinePlugin({
+      ETHEREUM_NODE_URL: JSON.stringify(process.env.ETHEREUM_NODE_URL),
+      IPFS_API_HOST: JSON.stringify(process.env.IPFS_API_HOST),
+      IPFS_API_PORT: JSON.stringify(process.env.IPFS_API_PORT),
+      IPFS_GATEWAY_URL: JSON.stringify(process.env.IPFS_GATEWAY_URL)
+    })
   ],
   module: {
     rules: [
@@ -32,5 +39,10 @@ module.exports = {
         }
       }
     ]
+  },
+  devServer:{
+    host: '0.0.0.0',
+    disableHostCheck:true,
+    public:'0.0.0.0'  
   }
 }
